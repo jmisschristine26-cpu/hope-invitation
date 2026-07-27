48e884b6da8a1a4fdf7cc205aa019c4b6e5a98be
jmisschristine26-cpu <jmisschristine26@gmail.com>
2026-07-27 18:59:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ alert("JavaScript is loading!");
 document.addEventListener("DOMContentLoaded", function () {
 
     const openButton = document.getElementById("openBook");
+   
+    console.log(openButton);
+
+alert(openButton ? "Button found!" : "Button NOT found!");
+    
     const storybook = document.getElementById("storybook");
 
     openButton.addEventListener("click", function () {
