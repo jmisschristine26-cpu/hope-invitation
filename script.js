ed46dfdd36f9b85e2b19bbd67298e1a505a2b590
jmisschristine26-cpu <jmisschristine26@gmail.com>
2026-07-27 18:12:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("script.js loaded!");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const openButton = document.getElementById("openBook");
+    const storybook = document.getElementById("storybook");
+
+    openButton.addEventListener("click", function () {
+
+        storybook.classList.remove("hidden");
+
+        storybook.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+
+});
