888c82de43f6ec6528e4ba88df8f2e1a2fe277c8
jmisschristine26-cpu <jmisschristine26@gmail.com>
2026-07-27 18:33:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     openButton.addEventListener("click", function () {
 
+        alert(`Welcome!
+
+We are delighted to invite you to celebrate Hope Jelizabeth Azaniah M. Frias' Christening and 1st Birthday.
+
+This is only the beginning of her storybook... 💖`);
+
         storybook.classList.remove("hidden");
 
         storybook.scrollIntoView({
